d419a862c3df56e5a8a7fde6cd0b08d27a033304
Aerendyll <143167807+Aerendyll@users.noreply.github.com>
2024-07-07 02:10:59 +0200

    changed svg img path

--- src/Pages/Home/Hero/index.jsx
+++ src/Pages/Home/Hero/index.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import HeroInfo from "./HeroInfo";
-import HeroWave from "../../../assets/svgs/wave3.svg"
+import HeroWave from "/src/assets/svgs/wave3.svg?t=1720309777909"
 import HeroImage from "./HeroImage";
 
 const Container = styled.div`
-  background-image: url("/src/assets/svgs/wave3.svg?t=1720309777909");
+  background-image: url(${HeroWave});
   background-repeat: no-repeat;
   background-size: cover;
   display: flex;
