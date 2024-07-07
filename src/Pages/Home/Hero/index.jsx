068edf05aa5701c7f2b5d8d3639b9639c4168ef8
Aerendyll <143167807+Aerendyll@users.noreply.github.com>
2024-07-07 02:33:10 +0200

    broken flex header

--- src/Pages/Home/Hero/index.jsx
+++ src/Pages/Home/Hero/index.jsx
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 import HeroInfo from "./HeroInfo";
-import HeroWave from "/src/assets/svgs/wave3.svg?t=1720309777909"
 import HeroImage from "./HeroImage";
+import HeroWave from "../../../assets/svgs/wave3.svg";
 
 const Container = styled.div`
   background-image: url(${HeroWave});
   background-repeat: no-repeat;
   background-size: cover;
   display: flex;
-  flex-wrap:wrap;
-  flex-direction:row;
+  flex-wrap: wrap;
+  flex-direction: row;
   justify-content: space-around;
   align-items: center;
   height: 80vh;
