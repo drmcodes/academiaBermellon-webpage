1a5dbfa5ef518bf365c68b3142a7e226812dfde1
= <=>
2024-07-18 13:01:40 +0200

    add hamburger button

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { useState } from "react";
+
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -10,18 +12,64 @@ const Container = styled.div`
   padding-right: 30px;
   font-size: 1em;
   padding: auto;
-  @media (max-width: 800px) {
-    display: none;
+   @media (max-width: 810px) {
+    justify-content: flex-end;
+  }
+`;
+
+const Button = styled.button`
+  background-color: var(--night);
+  color: var(--white-smoke);
+  margin: 20px;
+  border: none;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+   @media (max-width: 810px) {
+    display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+    position: absolute;
+    top: 50px;
+    right: 30px;
+    background-color: var(--night);
+    flex-direction: column;
+    width: 200px;
+    padding: 10px;
+    border-radius: 5px;
+  }
+`;
+
+const HamburgerButton = styled.button`
+  display: none;
+  background: none;
+  border: none;
+  cursor: pointer;
+
+  @media (max-width: 810px) {
+    display: flex;
+    color: var(--white-smoke);
+    font-size: 24px;
   }
 `;
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <Container>
-      <p className="nav1-p">Inicio</p>
-      <p className="nav1-p">Sobre nosotros</p>
-      <p className="nav1-p">Servicios</p>
-      <p className="nav1-p">Galería</p>
+      <HamburgerButton onClick={() => setIsOpen(!isOpen)}>
+        &#9776;
+      </HamburgerButton>
+      <Menu isOpen={isOpen}>
+        <Button>Inicio</Button>
+        <Button>Sobre nosotros</Button>
+        <Button>Servicios</Button>
+        <Button>Galería</Button>
+      </Menu>
     </Container>
   );
 };
