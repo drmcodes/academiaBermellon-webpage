10703ae6c3789a02228e4a0069fc6d89f4bc0773
= <=>
2024-07-19 11:50:19 +0200

    feat footer init

---
+++ src/Pages/Home/Footer/index.jsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
+  background-color:var(--eerie-black);
+  color:var(--white);
+  border-top:1px solid white;
+  
+`;
+const Content = styled.div`
+  display: grid;
+  width: 90%;
+  grid-template-columns: repeat(4, 1fr);
+  text-align: left;
+`;
+const FooterContent = styled.div`
+  margin: 10px;
+`;
+
+const Title = styled.h2`
+padding:0;
+width:
+`
+
+const Paragraph = styled.p`
+
+`
+
+const UList = styled.ul`
+list-style:none;
+margin:0;
+padding:0;
+`
+
+const Footer = () => {
+  return (
+    <Container>
+      <Content>
+        <FooterContent>
+          <Title>FOOTER CONTENT</Title>
+          <Paragraph>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+            quod hic suscipit voluptates corrupti placeat!
+          </Paragraph>
+        </FooterContent>
+        <FooterContent>
+          <Title>LINKS</Title>
+          <UList>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+            <li>Link 4</li>
+          </UList>
+        </FooterContent>
+        <FooterContent>
+          <Title>LINKS</Title>
+          <UList>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+            <li>Link 4</li>
+          </UList>
+        </FooterContent>
+        <FooterContent>
+          <Title>LINKS</Title>
+          <UList>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+            <li>Link 4</li>
+          </UList>
+        </FooterContent>
+      </Content>
+    </Container>
+  );
+};
+
+export default Footer;
